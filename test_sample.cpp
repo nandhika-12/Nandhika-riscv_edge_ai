fb38ec9b53cf9787a8bfc12534967a8f3edfddc7
nandhika-12 <nandhikapalanikumar@gmail.com>
2026-05-21 22:15:56 +0530

    Add files via upload

---
+++ test_sample.cpp
@@ -0,0 +1,20 @@
+// test_sample.h
+// ESP32 Test Sample Input for Student Score Prediction
+
+#ifndef TEST_SAMPLE_H
+#define TEST_SAMPLE_H
+
+#define NUM_FEATURES 5
+
+// Example student input data
+// Subjects: Maths, Science, English, Social, Computer
+
+double test_sample[NUM_FEATURES] = {
+  85.0,   // Maths
+  90.0,   // Science
+  78.0,   // English
+  88.0,   // Social
+  92.0    // Computer
+};
+
+#endif
